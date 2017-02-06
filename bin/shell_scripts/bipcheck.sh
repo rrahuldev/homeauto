ade14a5d69cbd666d2406c64a0c06e5815850f99
Rdpi <rrahuldev@gmail.com>
2017-02-06 00:08:39 -0600

    updates bip/nightmode

--- bin/shell_scripts/bipcheck.sh
+++ bin/shell_scripts/bipcheck.sh
@@ -4,5 +4,5 @@ old_bip=$(grep "broadlinkp2" secrets.yaml | cut -d' ' -f2)
 new_bip=$(ip neighbor | grep "b4:43:0d:ee:f4:fe" | cut -d" " -f1)
 if [ "$old_bip" != "$new_bip" ]; then
     sed -i "s/$old_bip/$new_bip/g" "secrets.yaml"
-    sudo systemctl restart home-assistant.service
+    sudo systemctl restart home-assistant@homeassistant.service
 fi;
