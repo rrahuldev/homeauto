7f3dbaafee2f1b0f8be2ad08c27ad49331c9671b
rrahuldev <rrahuldev@gmail.com>
2017-02-06 13:57:30 -0600

    updated scripts with log

--- bin/shell_scripts/bipcheck.sh
+++ bin/shell_scripts/bipcheck.sh
@@ -4,5 +4,6 @@ old_bip=$(grep "broadlinkp2" secrets.yaml | cut -d' ' -f2)
 new_bip=$(ip neighbor | grep "b4:43:0d:ee:f4:fe" | cut -d" " -f1)
 if [ "$old_bip" != "$new_bip" ]; then
     sed -i "s/$old_bip/$new_bip/g" "secrets.yaml"
+    echo "$old_bip changed to $new_bip" >> dotsh.log
     sudo systemctl restart home-assistant@homeassistant.service
 fi;
--- bin/shell_scripts/ha_update.sh
+++ bin/shell_scripts/ha_update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # script to update Home Assistant
+cd /home/homeassistant/.homeassistant
 sudo su -s /bin/bash homeassistant
 source /srv/homeassistant/homeassistant_venv/bin/activate
 pip3 install --upgrade homeassistant
 exit
-reha
+echo "Upgrade script complete, restarting hass" >> dotsh.log
